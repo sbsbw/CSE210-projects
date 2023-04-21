bf6075dd549f1678d5a2cf89a33b4ba611971ff7
sbsbsw <sbsbw@byui.edu>
2023-04-20 22:26:03 -0600

    Prep 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // First we will assign the variables.
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // We will now use the first and last name the user gave to tell them their name on the console.
+        Console.WriteLine($"\nYour name is {last}, {first} {last}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,46 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        // Promt user for their grade.
+        Console.Write("What is your grade? ");
+        string answer = Console.ReadLine();
+        float grade = float.Parse(answer);
+        
+        // Create a series of if statements to tell user thier grade.
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is {letter}.");
+
+        // Tell use whether they passed the class or not.
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine($"You failed the class.");
+        }
     }
 }
